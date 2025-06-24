34e816e4823a4d9f0b3de4108e557dbfcdb67e29
Manan Computer <sami606713@gmail.com>
2025-06-24 14:38:39 -0700

    Debugging the bugs2

--- vectorStore/vectorStore.py
+++ vectorStore/vectorStore.py
@@ -84,7 +84,7 @@ def GetContext(query: str, docs: List[Document]):
     )
 
     # Step 3: Retrieve and rerank
-    results = hybrid_retriever.invoke(query)
+    results = compression_retriever.invoke(query)
     
     source = [doc.metadata.get("source", "Unknown") for doc in results]
     # Step 4: Build response
